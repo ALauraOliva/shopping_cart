61ba693a1e34be0bc42ce81c94669d5fbdb4b4e7
Andrea Laura <andrea_carol00@hotmail.com>
2023-10-03 11:26:19 -0500

    store page 🏪
    creating and styling store page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <Navbar className="bg-white shadow-sm mb-3">
+    <Navbar sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Navbar.Brand href="/">Navbar</Navbar.Brand>
         <Nav className="me-auto">
@@ -20,7 +20,43 @@ const NavBar = () => {
             About
           </Nav.Link>
         </Nav>
-        <Button></Button>
+        <Button
+          variant="outline-primary"
+          className="rounded-circle"
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            stroke="currentColor"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+            <path d="M17 17h-11v-14h-2"></path>
+            <path d="M6 5l14 1l-1 7h-13"></path>
+          </svg>
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: "0",
+              right: "0",
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
       </Container>
     </Navbar>
   )
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,26 @@
-import React from "react"
+import storeItems from "../data/items.json"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import StoreItem from "../components/StoreItem"
 
 const Store = () => {
-  return <div>Store</div>
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem
+              id={item.id}
+              name={item.name}
+              price={item.price}
+              imgUrl={item.imgUrl}
+            />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default Store
